40048347654be2120c58f08a66958bc8b18d4e46
Patrik Ingverud <patrik_ingverud@hotmail.com>
2019-07-08 00:43:31 +0200

    remove unused imports

--- src/main/java/se/website/repository/InventoryRepository.java
+++ src/main/java/se/website/repository/InventoryRepository.java
@@ -1,7 +1,6 @@
 package se.website.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import se.website.domain.Product;
 
